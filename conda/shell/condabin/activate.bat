0669c08735f5a15f62dc3aaf761012bdf72858d5
Ken Odegard <kodegard@anaconda.com>
2024-04-23 14:38:11 -0500

    Cleanup

--- conda/shell/condabin/activate.bat
+++ conda/shell/condabin/activate.bat
@@ -2,5 +2,5 @@
 :: SPDX-License-Identifier: BSD-3-Clause
 
 @ECHO DeprecationWarning: 'activate' is deprecated. Use 'conda activate'. 1>&2
-@CALL "%~dp0\..\condabin\conda_hook.bat"
+@CALL "%~dp0\conda_hook.bat"
 conda activate %*
--- conda/shell/condabin/conda.bat
+++ conda/shell/condabin/conda.bat
@@ -3,18 +3,15 @@
 
 @IF NOT DEFINED CONDA_EXE (
   @FOR %%P IN ("%~dp0\..") DO @SET "__condaroot=%%~fP"
-
   @SET "CONDA_EXE=%__condaroot%\Scripts\conda.exe"
   @SET _CE_M=
   @SET _CE_CONDA=
-
   @SET __condaroot=
 )
 @IF [%1]==[activate]   "%~dp0\_conda_activate.bat" %*
 @IF [%1]==[deactivate] "%~dp0\_conda_activate.bat" %*
 
 @CALL "%CONDA_EXE%" %_CE_M% %_CE_CONDA% %*
-
 @IF NOT [%ERRORLEVEL%]==[0] EXIT /B %ERRORLEVEL%
 
 @IF [%1]==[install]   "%~dp0\_conda_activate.bat" reactivate
